126ddf1bce6b1d66ba32049dc134e34ddb7a0e42
Wesley <wesleykaihara@gmail.com>
2022-02-09 21:08:44 -0300

    [build]: Rodapé - Meios de Contato e Localização

---
+++ src/components/Footer/index.js
@@ -0,0 +1,25 @@
+import './style.css';
+
+export default function Footer() {
+  return (
+    <footer className='footer'>
+      <div></div>
+
+      <div className='footer_local'>
+
+        <div>
+          <p>Wesleykaihara@gmail.com</p>
+          <b>(44)99740-1925</b>
+        </div>
+
+        <hr />
+
+        <div>
+          <p>Av. Irmãos Pereira, 670 - Centro, Campo Mourão - PR, 87301-010</p>
+        </div>
+
+      </div>
+
+    </footer>
+  )
+}
