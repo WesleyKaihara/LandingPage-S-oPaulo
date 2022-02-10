517dcb5e814343f2d4b5e9e2c0e150b381492e5b
Wesley <wesleykaihara@gmail.com>
2022-02-10 11:13:05 -0300

    [docs]: referencia do video, troca icone e title

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -16,6 +16,7 @@ export default function Footer() {
 
         <div>
           <p>Av. Irmãos Pereira, 670 - Centro, Campo Mourão - PR, 87301-010</p>
+          <p className='referencia_content'>Video Utilizado: <a className="referencia" target="_blank" href="https://www.youtube.com/watch?v=-h2DNj-QaYY&list=LL&index=2&t=137s&ab_channel=OneManWolfPack" rel="noreferrer">One man Wolf Pack</a></p>
         </div>
 
       </div>
